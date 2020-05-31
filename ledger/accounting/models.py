9d2e55ced09f1b6c2b663484543e6961640cfeea
Mauro Javier Lopez <mauro.javier.lopez@gmail.com>
2020-05-31 20:28:17 -0300

    feat: add triggers for updates entries and account

--- ledger/accounting/models.py
+++ ledger/accounting/models.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
-# from django_extensions.db.models import TimeStampedModel
 from django_pgviews import view as pg
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
 from django.db import models
-
 
 # TODO: create and move to utils folders
 def validate_positive_amount(value: Decimal) -> bool:
@@ -53,3 +53,50 @@ class AccountLedgers(pg.View):
     class Meta:
         managed = False
         db_table = 'account_ledgers_view'
+
+VIEW_SQL_ACCOUNT_BALANCE = """
+    SELECT
+        account_id,
+        tdate AS timestamp,
+        sum(day_end) over (partition by account_id order by tdate asc) AS balance
+    FROM (
+        SELECT
+            accounting_account.id as account_id,
+            account_ledgers_view.timestamp::date as tdate,
+            COALESCE(sum(account_ledgers_view.amount), 0.0) as day_end
+        FROM
+            accounting_account
+            LEFT OUTER JOIN
+            account_ledgers_view
+            ON accounting_account.id = account_ledgers_view.account_id
+        GROUP BY accounting_account.id, account_ledgers_view.timestamp::date
+    ) AS daily_balances;
+    """
+
+class AccountBalance(pg.MaterializedView):
+    concurrent_index = 'account_id, timestamp'
+    account = models.ForeignKey(Account, primary_key=True, on_delete=models.PROTECT)
+    timestamp = models.DateTimeField()
+    balance = models.DecimalField(max_digits=20, decimal_places=2)
+
+    sql = VIEW_SQL_ACCOUNT_BALANCE
+
+    class Meta:
+        managed = False
+        db_table = 'account_balances'
+
+@receiver(post_save, sender=Entries)
+def trigger_fix_balance_entries_on_save(sender, action=None, instance=None, **kwargs):
+    AccountBalance.refresh()
+
+@receiver(post_delete, sender=Entries)
+def trigger_fix_balance_entries_on_delete(sender, action=None, instance=None, **kwargs):
+    AccountBalance.refresh()
+
+@receiver(post_save, sender=Account)
+def trigger_fix_balance_accounts_on_save(sender, action=None, instance=None, **kwargs):
+    AccountBalance.refresh()
+
+@receiver(post_delete, sender=Account)
+def trigger_fix_balance_accounts_on_delete(sender, action=None, instance=None, **kwargs):
+    AccountBalance.refresh()
